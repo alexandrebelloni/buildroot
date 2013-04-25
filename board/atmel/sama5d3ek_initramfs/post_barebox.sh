eae995d165e51cd0cd4622dc000cde8cfa22dbf2
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2013-04-25 18:11:58 +0200

    Boottime optimization data and configs
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

---
+++ board/atmel/sama5d3ek_initramfs/post_barebox.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mv ${1}/sbin/* ${1}/usr/bin/* ${1}/usr/sbin/* ${1}/bin/
+rm -rf ${1}/{etc,sbin,usr,var,lib,linuxrc}
+
+cat << EOF > ${1}/init
+#!/bin/sh
+echo "Booted"
+
+mount -t sysfs none /sys
+ubiattach -m 6
+mount -t ubifs ubi0:rootfs /newroot
+umount /sys
+exec switch_root /newroot/ /init
+EOF
+
